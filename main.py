39a7be3d24a8c2ea2c92671e93e1db6eb465a35e
Alan Friedman <d.alan.friedman@gmail.com>
2023-01-14 12:07:40 -0500

    Log to file

--- main.py
+++ main.py
@@ -4,12 +4,29 @@ import os
 import openai
 import json
 import argparse, sys
+import logging
+# from datetime import datetime
+import uuid
+
+id = uuid.uuid4()
+
+# Modified from: https://stackoverflow.com/a/17558764/5079258
+extra = {'id': id}
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s - %(id)s - %(message)s',  '%m/%d/%Y %I:%M:%S %p')
+logger.setLevel(logging.DEBUG)
+fileHandler = logging.FileHandler('chatgpt.log')
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+logger = logging.LoggerAdapter(logger, extra)
+
+logger.info("Starting...")
 
 parser=argparse.ArgumentParser()
 
-parser.add_argument("prompt", help="ChatGPT Prompt")
-parser.add_argument("--temp", help="Temperature", default=0.9)
-parser.add_argument("--maxTokens", help="Max tokens", default=48)
+parser.add_argument("--prompt", help="Prompt")
+parser.add_argument("--temp", help="Temperature", type=int, default=0.9)
+parser.add_argument("--maxTokens", help="Max tokens", type=int, default=48)
 
 args=parser.parse_args()
 
@@ -20,6 +37,8 @@ if args.prompt:
 else:  
   prompt = input("Enter prompt: ")
 
+logger.info(f"Prompt: {prompt} - Temperature: {args.temp} - Max tokens: {args.maxTokens}")
+
 response = openai.Completion.create(
   model="text-davinci-003",
   prompt=prompt,
@@ -27,4 +46,20 @@ response = openai.Completion.create(
   max_tokens=args.maxTokens
 )
 
-print(response.choices[0].text.strip())
+textResponse = response.choices[0].text.strip()
+
+logger.info(f"Response: {textResponse}")
+print(textResponse)
+
+prompt_tokens = response.usage.prompt_tokens
+completion_tokens = response.usage.completion_tokens
+total_tokens = response.usage.total_tokens
+
+print("\n---\n")
+
+usageLog = f"Token usage - Prompt: {prompt_tokens} - Completion: {completion_tokens} - Total: {total_tokens}"
+
+logger.info(usageLog)
+print(usageLog)
+
+logger.info("Done.")
